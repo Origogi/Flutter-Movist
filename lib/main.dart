a1ba453b568ca7b3896646c6291e63ff52e42a5f
gimjeongtae <rlawjdxo1212@naver.com>
2019-09-22 20:24:19 +0900

    third

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_list/data.dart';
 import 'CustomIcon.dart';
+import 'dart:math';
 
 void main() => runApp(MaterialApp(
       home: MyApp(),
@@ -9,14 +11,27 @@ void main() => runApp(MaterialApp(
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _MyAppState();
   }
 }
 
+var cardAspectRatio = 12.0/16.0;
+var widgetAspectRatio = cardAspectRatio * 1.2;  
+
 class _MyAppState extends State<MyApp> {
+
+  var currentPage = images.length -1.0;
+
   @override
   Widget build(BuildContext context) {
+    PageController controller = PageController(initialPage: images.length -1);
+
+    controller.addListener(() {
+      setState(() {
+        currentPage = controller.page;      
+      });
+    });
+
     return Scaffold(
       backgroundColor: Color(0xFF2d3447),
       body: SingleChildScrollView(
@@ -86,13 +101,92 @@ class _MyAppState extends State<MyApp> {
                         ),
                       ),
                     ),
-                  )
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Text('25+ stories', style : TextStyle(color: Colors.blueAccent))
                 ],
               ),
+            ),
+            Stack(
+              children: <Widget>[
+                CardControllWidget(currentPage),
+                Positioned.fill(
+                  child: PageView.builder(
+                    itemCount: images.length,
+                    controller: controller,
+                    reverse: true,
+                    itemBuilder: (context,index) {
+                      return Container();
+                    },
+                  ),
+                )
+              ],
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class CardControllWidget extends StatelessWidget {
+var currentPage;
+var padding = 20.0;
+var verticalInset = 20.0;
+
+  CardControllWidget(this.currentPage);
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: widgetAspectRatio,
+      child: LayoutBuilder(builder: (context, constraints) {
+        var width = constraints.maxWidth;
+        var height = constraints.maxHeight;
+
+        var safeWidth = width - 2 * padding;
+        var safeHeight = height - 2 * padding;
+
+        var heightOfPrimaryCard = safeHeight;
+        var widthOfPrimaryCard = heightOfPrimaryCard * cardAspectRatio;
+
+        var primaryCardLeft = safeWidth - widthOfPrimaryCard;
+        var horizontalInset = primaryCardLeft /2;
+
+        List<Widget> cardList = List();
+
+        for (var i =0; i< images.length;i++) {
+          var delta = i -currentPage;
+          bool isOnRight = delta > 0;
+
+          var start = padding + max(
+            primaryCardLeft - horizontalInset * -delta * (isOnRight ? 15 : 1), 0.0);
+
+          var cardItem = Positioned.directional(
+            top: padding + verticalInset * max(-delta,0.0),
+            bottom: padding + verticalInset * max(-delta, 0.0),
+            start: start,
+            textDirection: TextDirection.rtl,
+            child: Container(
+              child: AspectRatio(
+                aspectRatio: cardAspectRatio,
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: <Widget>[
+                    Image.asset(images[i], fit : BoxFit.cover)
+                  ],
+                ),
+              ),
+            ),
+          );
+          cardList.add(cardItem);
+        }
+        return Stack(
+          children: cardList,
+        );
+      },),
     );
   }
 }
