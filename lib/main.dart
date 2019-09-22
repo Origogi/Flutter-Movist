edf08acd8ac0acd709f6690fa4c93bfcd0155b17
gimjeongtae <rlawjdxo1212@naver.com>
2019-09-22 21:21:25 +0900

    Complete tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -15,20 +15,19 @@ class MyApp extends StatefulWidget {
   }
 }
 
-var cardAspectRatio = 12.0/16.0;
-var widgetAspectRatio = cardAspectRatio * 1.2;  
+var cardAspectRatio = 12.0 / 16.0;
+var widgetAspectRatio = cardAspectRatio * 1.2;
 
 class _MyAppState extends State<MyApp> {
-
-  var currentPage = images.length -1.0;
+  var currentPage = images.length - 1.0;
 
   @override
   Widget build(BuildContext context) {
-    PageController controller = PageController(initialPage: images.length -1);
+    PageController controller = PageController(initialPage: images.length - 1);
 
     controller.addListener(() {
       setState(() {
-        currentPage = controller.page;      
+        currentPage = controller.page;
       });
     });
 
@@ -85,7 +84,7 @@ class _MyAppState extends State<MyApp> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left :20.0),
+              padding: const EdgeInsets.only(left: 20.0),
               child: Row(
                 children: <Widget>[
                   Container(
@@ -94,7 +93,8 @@ class _MyAppState extends State<MyApp> {
                         borderRadius: BorderRadius.circular(20.0)),
                     child: Center(
                       child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 22.0, vertical: 6.0),
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 22.0, vertical: 6.0),
                         child: Text(
                           'Animated',
                           style: TextStyle(color: Colors.white),
@@ -105,7 +105,8 @@ class _MyAppState extends State<MyApp> {
                   SizedBox(
                     width: 15,
                   ),
-                  Text('25+ stories', style : TextStyle(color: Colors.blueAccent))
+                  Text('25+ stories',
+                      style: TextStyle(color: Colors.blueAccent))
                 ],
               ),
             ),
@@ -117,9 +118,72 @@ class _MyAppState extends State<MyApp> {
                     itemCount: images.length,
                     controller: controller,
                     reverse: true,
-                    itemBuilder: (context,index) {
+                    itemBuilder: (context, index) {
                       return Container();
                     },
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Favourite',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 46.0,
+                          fontFamily: 'Calibre-Semibold',
+                          letterSpacing: 1.0)),
+                  IconButton(
+                    icon: Icon(
+                      Icons.more_horiz,
+                      color: Colors.white,
+                      size: 30.0,
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: Center(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 22.0, vertical: 6.0),
+                        child: Text(
+                          'Lastest',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Text('9+ stories', style: TextStyle(color: Colors.blueAccent))
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: 18.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20.0),
+                    child: Image.asset('assets/image_02.jpg',
+                        width: 296.0, height: 222.0),
                   ),
                 )
               ],
@@ -132,9 +196,9 @@ class _MyAppState extends State<MyApp> {
 }
 
 class CardControllWidget extends StatelessWidget {
-var currentPage;
-var padding = 20.0;
-var verticalInset = 20.0;
+  var currentPage;
+  var padding = 20.0;
+  var verticalInset = 20.0;
 
   CardControllWidget(this.currentPage);
 
@@ -142,51 +206,102 @@ var verticalInset = 20.0;
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: widgetAspectRatio,
-      child: LayoutBuilder(builder: (context, constraints) {
-        var width = constraints.maxWidth;
-        var height = constraints.maxHeight;
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          var width = constraints.maxWidth;
+          var height = constraints.maxHeight;
 
-        var safeWidth = width - 2 * padding;
-        var safeHeight = height - 2 * padding;
+          var safeWidth = width - 2 * padding;
+          var safeHeight = height - 2 * padding;
 
-        var heightOfPrimaryCard = safeHeight;
-        var widthOfPrimaryCard = heightOfPrimaryCard * cardAspectRatio;
+          var heightOfPrimaryCard = safeHeight;
+          var widthOfPrimaryCard = heightOfPrimaryCard * cardAspectRatio;
 
-        var primaryCardLeft = safeWidth - widthOfPrimaryCard;
-        var horizontalInset = primaryCardLeft /2;
+          var primaryCardLeft = safeWidth - widthOfPrimaryCard;
+          var horizontalInset = primaryCardLeft / 2;
 
-        List<Widget> cardList = List();
+          List<Widget> cardList = List();
 
-        for (var i =0; i< images.length;i++) {
-          var delta = i -currentPage;
-          bool isOnRight = delta > 0;
+          for (var i = 0; i < images.length; i++) {
+            var delta = i - currentPage;
+            bool isOnRight = delta > 0;
 
-          var start = padding + max(
-            primaryCardLeft - horizontalInset * -delta * (isOnRight ? 15 : 1), 0.0);
+            var start = padding +
+                max(
+                    primaryCardLeft -
+                        horizontalInset * -delta * (isOnRight ? 15 : 1),
+                    0.0);
 
-          var cardItem = Positioned.directional(
-            top: padding + verticalInset * max(-delta,0.0),
-            bottom: padding + verticalInset * max(-delta, 0.0),
-            start: start,
-            textDirection: TextDirection.rtl,
-            child: Container(
-              child: AspectRatio(
-                aspectRatio: cardAspectRatio,
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: <Widget>[
-                    Image.asset(images[i], fit : BoxFit.cover)
-                  ],
+            var cardItem = Positioned.directional(
+              top: padding + verticalInset * max(-delta, 0.0),
+              bottom: padding + verticalInset * max(-delta, 0.0),
+              start: start,
+              textDirection: TextDirection.rtl,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(16.0),
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                    BoxShadow(
+                        color: Colors.black,
+                        offset: Offset(3.9, 6.0),
+                        blurRadius: 10.0)
+                  ]),
+                  child: AspectRatio(
+                    aspectRatio: cardAspectRatio,
+                    child: Stack(
+                      fit: StackFit.expand,
+                      children: <Widget>[
+                        Image.asset(images[i], fit: BoxFit.cover),
+                        Align(
+                          alignment: Alignment.bottomLeft,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 16.0, vertical: 10.0),
+                                child: Text(title[i],
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 25.0,
+                                        fontFamily: 'SF-Pro-Text-Regular')),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 12.0, bottom: 12.0),
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 22.0, vertical: 6.0),
+                                  decoration: BoxDecoration(
+                                      color: Colors.blueAccent,
+                                      borderRadius:
+                                          BorderRadius.circular(20.0)),
+                                  child: Text(
+                                    'Read Later',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
+            );
+            cardList.add(cardItem);
+          }
+          return Stack(
+            children: cardList,
           );
-          cardList.add(cardItem);
-        }
-        return Stack(
-          children: cardList,
-        );
-      },),
+        },
+      ),
     );
   }
 }
