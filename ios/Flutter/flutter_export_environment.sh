4c46f454e8057651182a4009f8ddb1564ca62ddd
gimjeongtae <rlawjdxo1212@naver.com>
2019-10-04 23:41:09 +0900

    Fix url

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\User\Documents\flutter_movie_list"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/gimjeongtae/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/gimjeongtae/Documents/dart_language_tour/flutter_list"
+export "FLUTTER_TARGET=/Users/gimjeongtae/Documents/dart_language_tour/flutter_list/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/gimjeongtae/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
